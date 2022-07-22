deb88d55ce0b7d41d8198152fc87a8b540b3078f
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-07-22 20:12:18 -0300

    chore: dandao um update no visual

--- src/components/ListProducts.js
+++ src/components/ListProducts.js
@@ -7,17 +7,17 @@ export default function ListProducts() {
     page,
   } = useContext(ProductsContext);
 
+
   return (
     <div className="flex-col justify-center items-center mt-6 md:grid md:grid-cols-2 md:gap-2">
-      {dataFilter.length > 0 && (
-        dataFilter.slice(0, page).map((item) => (
-          <div key={item.id} className="ring-1 max-w-sm w-80 mx-auto mt-4 max-h-fit bg-slate-300 rounded-xl shadow-lg md:max-w-2xl">
+      {dataFilter.slice(0, page).map((item) => (
+          <div key={item.id} className="motion-safe:animate-fadeIn ring-1 max-w-sm w-80 mx-auto mt-4 max-h-fit bg-slate-300 rounded-xl shadow-lg md:max-w-2xl">
             <div className="md:flex flex-col items-center justify-center">
               <div className="mt-4 rounded-tl-xl rounded-b-sm h-10 flex justify-center items-center border-b-2 w-full">
                 <h2 className="text-center text-white font-bold">{item.name}</h2>
               </div>
-              <div className="md:shrink-0 mt-8 rounded-md flex items-center justify-center bg-contain">
-                <img className="w-fit mx-auto mt-4 items-center object-cover ring-1 bg-white rounded-md md:w-full h-40" src={item.images[0].asset.url} alt={item.images[0].alt} />
+              <div className="motion-safe:animate-fadeIn md:shrink-0 mt-8 rounded-md flex items-center justify-center bg-contain">
+                <img className="motion-safe:animate-fadeIn w-fit mx-auto mt-4 items-center object-cover ring-1 bg-white rounded-md md:w-full h-40" src={item.images[0].asset.url} alt={item.images[0].alt} />
               </div>
               <div className="w-80 mx-auto max-h-32 p-8 mb-6">
                 <details className="  open:ring-1 p-6 rounded-lg">
@@ -34,8 +34,7 @@ export default function ListProducts() {
               </div>
             </div>
           </div>
-        ))
-      )}
+        ))}
     </div>
   );
 }
