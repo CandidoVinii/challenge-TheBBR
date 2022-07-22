86528ac3472910294fa09944ce2ed65f0418469b
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-07-21 21:55:18 -0300

    Feat: adicionado tailwind e configurado

--- src/pages/MainScreen.js
+++ src/pages/MainScreen.js
@@ -5,17 +5,18 @@ export default function MainScreen() {
   return (
     <div>
       <header>
-        <div>
-          <h1>The Brooklyn Brothers Shop</h1>
+        <div className="rounded-sm bg-zinc-800 h-20 flex items-center justify-center">
+          <h1 className="bg-[#e53262] w-fit h-fit rounded-md text-3xl font-bold underline">
+            The Brooklyn Brothers Shop</h1>
         </div>
-        <div>
+        <div className="flex items-center justify-center">
           <FilterHeader />
         </div>
       </header>
-      <main>
+      <main className="flex items-center justify-center">
         <ListProducts />
       </main>
-      <footer>
+      <footer className="bg-[#e53262] border-black border-2 mt-10 flex justify-center">
         Desenvolvido com muito Amor
       </footer>
     </div>
