cb12db1a25b9c89505803a164111a8b00127a1e9
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-07-22 15:46:43 -0300

    chore: adicionado um estilo mais simples e mais limpo

--- src/components/FilterHeader.js
+++ src/components/FilterHeader.js
@@ -14,22 +14,22 @@ export default function FilterHeader() {
       <form className="flex">
         <input
           placeholder="Digite sua pesquisa aqui..."
-          className="shadow appearance-none border-none rounded w-full py-2 px-3 h-8 bg-zinc-200 leading-tight focus:outline-none focus:shadow-outline"
+          className="focus:border-sky-500 focus:ring-1 focus:ring-sky-500 shadow-md border-none rounded w-full py-2 px-3 h-8 bg-slate-300 leading-tight focus:outline-none focus:shadow-outline"
           onChange={handleChange}
           type="text" />
         <select
-          className="text-center ml-4 shadow appearance-none border-none rounded w-fit py-2 px-3 h-8 text-gray-400 bg-zinc-200 leading-tight focus:outline-none focus:shadow-outline"
+          className="focus:border-sky-500 focus:ring-1 focus:ring-sky-500 text-center ml-4 shadow appearance-none border-none rounded w-fit py-2 px-3 h-8 text-gray-800 bg-slate-300 leading-tight focus:outline-none focus:shadow-outline"
           name="category"
           label="Select Category"
           id="category"
           value={filter}
           onChange={({ target }) => setFilter(target.value)}
         >
-          <option className="text-gray-400" value="all">All</option>
+          <option className="text-gray-800" value="all">All</option>
           {filterCategory !== undefined && (
               filterCategory.map((item, index) => (
                 <option
-                  className="text-gray-400"
+                  className="text-gray-800"
                   key={index}
                   value={item}>
                   {item}
--- src/components/ListProducts.js
+++ src/components/ListProducts.js
@@ -11,16 +11,16 @@ export default function ListProducts() {
     <div className="flex-col justify-center items-center mt-6 md:grid md:grid-cols-2 md:gap-2">
       {dataFilter.length > 0 && (
         dataFilter.slice(0, page).map((item) => (
-          <div key={item.id} className="ring-red-400 ring-1 max-w-sm w-80 mx-auto mt-4 max-h-fit bg-zinc-900 rounded-xl shadow-md md:max-w-2xl">
+          <div key={item.id} className="ring-1 max-w-sm w-80 mx-auto mt-4 max-h-fit bg-slate-300 rounded-xl shadow-lg md:max-w-2xl">
             <div className="md:flex flex-col items-center justify-center">
-              <div className="mt-4 rounded-tl-xl rounded-b-sm h-10 flex justify-center items-center border-b-2 w-full border-red-400">
+              <div className="mt-4 rounded-tl-xl rounded-b-sm h-10 flex justify-center items-center border-b-2 w-full">
                 <h2 className="text-center text-white font-bold">{item.name}</h2>
               </div>
-              <div className="md:shrink-0 mt-8 rounded-md flex items-center justify-center">
-                <img className="w-fit object-cover ring-1 bg-white  ring-red-400 rounded-md md:w-full h-40" src={item.images[0].asset.url} alt={item.images[0].alt} />
+              <div className="md:shrink-0 mt-8 rounded-md flex items-center justify-center bg-contain">
+                <img className="w-fit mx-auto mt-4 items-center object-cover ring-1 bg-white rounded-md md:w-full h-40" src={item.images[0].asset.url} alt={item.images[0].alt} />
               </div>
               <div className="w-80 mx-auto max-h-32 p-8 mb-6">
-                <details className=" open:ring-red-400 open:ring-1 p-6 rounded-lg">
+                <details className="  open:ring-1 p-6 rounded-lg">
                   <summary className="text-sm text-white text-center leading-6 font-semibold select-none">
                     Description
                   </summary>
@@ -29,7 +29,7 @@ export default function ListProducts() {
                   </div>
                 </details>
               </div>
-              <div className="bg-red-400 w-fit text-center mx-auto rounded-lg">
+              <div className="bg-zinc-50 w-fit text-center mx-auto rounded-lg">
                 <span className="font-bold font-mono">{`Category: ${item.category.name}`}</span>
               </div>
             </div>
