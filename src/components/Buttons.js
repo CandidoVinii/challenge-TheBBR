d69ea67cb38d1d2e3b25c47e6d0b687282d29340
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-07-25 16:22:42 -0300

    style: corrigido erros de espaços

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -71,7 +71,6 @@ export default function Buttons() {
       >
         <FaArrowCircleUp />
       </button>
-
     </div>
   );
 }
--- src/components/ListProducts.js
+++ src/components/ListProducts.js
@@ -1,18 +1,11 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import ProductsContext from '../context/ProductsContext';
 
-export default function ListProducts() {
-  const [showDescription, setShow] = useState(false);
-  
+export default function ListProducts() {  
   const {
     dataFilter,
     page,
   } = useContext(ProductsContext);
-
-  const handleClick = () => {
-    setShow(true);
-    console.log(oi);
-  }
 
   return (
     <div className="motion-safe:animate-fadeIn md:grid w-fit md:grid-cols-2 md:gap-2 lg:grid lg:grid-cols-2 lg:gap-4 2xl:grid-cols-4">
@@ -24,7 +17,7 @@ export default function ListProducts() {
           <div className="rounded-md flex items-center justify-center bg-contain">
             <img className="motion-safe:animate-fadeIn w-fit mx-auto mt-4 items-center object-cover ring-1 bg-white rounded-md md:w-full h-36" src={item.images[0].asset.url} alt={item.images[0].alt} />
           </div>
-          {/* <div className="max-w-[280px] w-80 sm:max-w-fit mx-auto max-h-32 p-8 mb-6">
+          <div className="max-w-[280px] w-80 sm:max-w-fit mx-auto max-h-32 p-8 mb-6">
             <details className=" open:ring-1 p-6 rounded-lg">
               <summary className="text-sm text-black text-center leading-6 font-semibold select-none">
                 Description:
@@ -33,14 +26,8 @@ export default function ListProducts() {
                 <p className="text-[8px] font-bold font-serif  text-ellipsis overflow-hidden">{item.shortDescription}</p>
               </div>
             </details>
-          </div> */}
+          </div>
           <div className="flex justify-center items-center mt-2">
-            <button
-              className="hover:bg-blue-400 transition-colors hover:shadow-md hover:shadow:black ring-2 italic semi-bold font-mono rounded-sm py-2 px-4"
-              onClick={handleClick}
-            >
-              Hide Description
-            </button>
           </div>
           <div className="mt-4 mb-2 w-fit text-center mx-auto rounded-lg ring-2 ring-color-red-500 rounded0md">
             <span className="font-bold font-mono">{`Category: ${item.category.name}`}</span>
